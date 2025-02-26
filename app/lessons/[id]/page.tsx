1c4863d0f52062897298e396b4990eafa2f8aa82
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-02-26 18:44:08 +0100

    🎨 design: floating player

--- app/lessons/[id]/page.tsx
+++ app/lessons/[id]/page.tsx
@@ -20,10 +20,8 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
   return (		
 		<main className="h-full w-full space-y-10 text-white overflow-scroll pt-24 pb-10 px-32">
 
-
 			{/* Title and tag */}
 			<section className="space-y-3">
-
 				<div className="flex justify-between items-center">
 
 					<div className="flex items-center space-x-3">
@@ -44,61 +42,58 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
 						<Tag key={index} content={tag} />
 					))}
 				</div>
-
 			</section>
 
+			<div className="space-y-10">
 
-			{/* Description */}
-			<section>
-				<p className="opacity-75">
-					{lesson.description}
-				</p>
-			</section>
+				{/* Audio player */}
+				<section className="float-right ml-10 mb-10">
+					<Player />
+				</section>
 
-			{/* Advices */}
-			<section className="space-y-3">
-				<h2 className="font-semibold text-2xl">
-					Conseils
-				</h2>
-				<p className="opacity-75">
-					Pour tirer le meilleur parti de cette séance, créez un espace calme et confortable. Éteignez les distractions et assurez-vous d&apos;être dans une posture adaptée, que ce soit sur un coussin ou une chaise.
-				</p>
-			</section>
+				{/* Description */}
+				<section>
+					<p className="opacity-75">
+						{lesson.description}
+					</p>
+				</section>
 
+				{/* Advices */}
+				<section className="space-y-3">
+					<h2 className="font-semibold text-2xl">
+						Conseils
+					</h2>
+					<p className="opacity-75">
+					Pour tirer pleinement profit de cette séance de méditation, il est essentiel de préparer un environnement dédié à la détente. Commencez par choisir un espace calme, loin du tumulte quotidien, où la lumière est douce et tamisée. Idéalement, optez pour une pièce peu exposée aux bruits extérieurs et assurez-vous que la température y est agréable, afin de créer une atmosphère propice à la relaxation. Prenez le temps de désactiver vos appareils électroniques et d&apos;éloigner toute source de distraction, comme la télévision ou les notifications de votre smartphone, afin de permettre à votre esprit de se recentrer. Vous pouvez également ajouter une touche personnelle en diffusant de la musique d&apos;ambiance apaisante ou en allumant une bougie parfumée pour instaurer une atmosphère sereine. Ensuite, installez-vous dans une posture confortable qui soutient votre dos, que ce soit sur un coussin de méditation posé sur le sol ou sur une chaise ergonomique, afin que votre corps soit à l&apos;aise et que votre esprit puisse se concentrer sur le moment présent. Prenez quelques instants pour vous ajuster, respirer profondément et vous préparer mentalement à l&apos;expérience qui vous attend. Ce rituel de préparation permet non seulement de réduire les tensions physiques et mentales, mais aussi de signaler à votre cerveau que vous entrez dans un espace de pleine présence et de sérénité, maximisant ainsi les bienfaits de la méditation.
+					</p>
+				</section>
 
-			{/* Audio player */}
-			<section className="space-y-3">
-				<h2 className="font-semibold text-2xl">
-					La leçon
-				</h2>
-				<Player />
-			</section>
-
-			{/* Comments */}
-			<section className="space-y-3">
-				<h2 className="font-semibold text-2xl">
-					À propos de l&apos;instructeur
-				</h2>
-				<div className="flex space-x-5">
-					<div className="aspect-square shrink-0">
-						<Image 
-							src="/pp.png"
-							alt="Marie Dupont"
-							width={90} height={90}
-							className="rounded-full"
-						/>
+				{/* Comments */}
+				<section className="space-y-3">
+					<h2 className="font-semibold text-2xl">
+						À propos de l&apos;instructeur
+					</h2>
+					<div className="flex space-x-5">
+						<div className="aspect-square shrink-0">
+							<Image 
+								src="/pp.png"
+								alt="Marie Dupont"
+								width={90} height={90}
+								className="rounded-full"
+							/>
+						</div>
+						<div className="flex flex-col justify-center">
+							<h3 className="font-semibold text-ld">
+								Marie Dupont
+							</h3>
+							<p className="opacity-75">
+								Instructrice certifiée en méditation guidée, forte de plus de 10 ans d&apos;expérience, spécialisée dans les techniques de pleine conscience pour la réduction du stress.
+							</p>
+						</div>
 					</div>
-					<div className="flex flex-col justify-center">
-						<h3 className="font-semibold text-ld">
-							Marie Dupont
-						</h3>
-						<p className="opacity-75">
-							Instructrice certifiée en méditation guidée, forte de plus de 10 ans d&apos;expérience, spécialisée dans les techniques de pleine conscience pour la réduction du stress.
-						</p>
-					</div>
-				</div>
-			</section>
-
+				</section>				
+			</div>
+		
 		</main>
   );
 }
--- app/ui/lessons/lesson/player.tsx
+++ app/ui/lessons/lesson/player.tsx
@@ -9,7 +9,7 @@ export function Player({ className }: PlayerProps) {
   return (
     <div className={clsx(
       className,
-      "flex flex-col items-center justify-center gap-4 bg-slate-700 bg-opacity-45 rounded-lg pt-8 overflow-hidden"
+      "flex flex-col items-center justify-center gap-5 rounded-lg pt-8 overflow-hidden max-w-max bg-slate-700 bg-opacity-45 aspect-3/4"
     )}>
 
       <Orb 
@@ -18,16 +18,16 @@ export function Player({ className }: PlayerProps) {
         hue={70}
         forceHoverState={true}
       />
-      <div className='flex flex-col items-center gap-1'>
+
+      <div className='flex flex-col items-center gap-1 mt-4'>
         <h2 className='text-white text-2xl font-semibold'>Écoutez</h2>
-        <p className='text-white text-sm opacity-50'>Durée: 15 minutes</p>
-      </div>
+        <p className='text-white text-sm opacity-50 pb-5 '>Durée: 15 minutes</p>
 
-      <div className='flex gap-4 pb-5'>
-        <button className='bg-white text-slate-700 px-4 py-2 rounded-lg'>Reprendre</button>
-        <button className='bg-white text-slate-700 px-4 py-2 rounded-lg'>Recommencer</button>
+        <div className='flex gap-4'>
+          <button className='bg-white text-slate-700 px-4 py-2 rounded-lg'>Reprendre</button>
+          <button className='bg-white text-slate-700 px-4 py-2 rounded-lg'>Recommencer</button>
+        </div>
       </div>
-
 
       <div className='w-full bg-white bg-opacity-20 rounded-full h-1'>
         <div className='bg-white rounded-full h-1 w-4/5'></div>  
