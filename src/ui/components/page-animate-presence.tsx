dbf2c7375979d8a58d63f78ae575383ee42013b8
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-03 18:00:36 +0100

    🎨 design: adding transition on leaving page

---
+++ src/ui/components/page-animate-presence.tsx
@@ -0,0 +1,37 @@
+'use client'
+
+import { AnimatePresence, motion } from 'motion/react'
+import { LayoutRouterContext } from 'next/dist/shared/lib/app-router-context.shared-runtime'
+import { useContext, useRef } from 'react'
+import { usePathname } from 'next/navigation'
+
+
+export const FrozenRoute = ({ children }: { children: React.ReactNode }) => {
+  const context = useContext(LayoutRouterContext)
+  const frozen = useRef(context).current
+
+  return <LayoutRouterContext.Provider value={frozen}>{children}</LayoutRouterContext.Provider>
+}
+
+export default function PageAnimatePresence(
+  { children } : { children: React.ReactNode }) {
+  const pathname = usePathname()
+  return (
+    <AnimatePresence mode="wait">
+      {/**
+       * We use `motion.div` as the first child of `<AnimatePresence />` Component so we can specify page animations at the page level.
+       * The `motion.div` Component gets re-evaluated when the `key` prop updates, triggering the animation's lifecycles.
+       * During this re-evaluation, the `<FrozenRoute />` Component also gets updated with the new route components.
+       */}
+      <motion.div 
+        key={pathname}
+        initial={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -200 }}
+        transition={{ duration: 1 }}
+      >
+        {/* {children} */}
+        <FrozenRoute>{children}</FrozenRoute>
+      </motion.div>
+    </AnimatePresence>
+  )
+}
