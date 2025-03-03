04987a0d3e4a3f88448604c9b2c0a6b37abc176c
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-03 18:08:07 +0100

    🎨 design: animate page transition

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import { Metadata } from 'next';
 
 import "@/src/ui/global.css";
-
+import PageAnimatePresence from '@/src/ui/components/page-animate-presence';
 import { Background } from "@/src/ui/components/background/background";
 import { primaryFont } from '@/src/ui/fonts';
 
-
-// Some metadata for the app
 export const metadata: Metadata = {
   title: {
     template: '%s | Harmonia',
@@ -17,12 +15,15 @@ export const metadata: Metadata = {
 
 // Main layout component
 export default function RootLayout({children}: { children: React.ReactNode; }) {
+
   return (
-    <html lang="en">
+    <html lang="fr">
       <body className={`${primaryFont.className} antialiased bg-gradient-to-br from-stone-600 to-stone-700`}>
 
         <div className="relative z-10">
-          {children}
+          <PageAnimatePresence>
+            {children}
+          </PageAnimatePresence>
         </div>
 
         <Background/>
