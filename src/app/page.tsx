d89dbb045445eba08e147c08a09bd1fb37796b77
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-03 18:03:53 +0100

    🎨 design: animate the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,31 +3,31 @@ import * as motion from 'motion/react-client';
 import { UnderlineButton } from '@/src/ui/components/buttons';
 import Logo from '@/src/ui/components/logo';
 
-
 // Root page
 export default function Page() {
   return (
+    
     <main className="flex min-h-screen flex-col p-6 items-center justify-center main">
       <div className="text-center max-w-screen-sm mx-auto text-xl space-y-10 text-stone-100">
         <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 2, delay: 0 }}
         >
           <Logo />
         </motion.div>
 
         <motion.p
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 2, delay: 0.5 }}
         >
           Bienvenue sur Harmonia. Découvrez une application de sophrologie et de méditation conçue pour vous aider à retrouver calme et équilibre, où que vous soyez.
         </motion.p>
 
         <motion.div
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
+          initial={{ opacity: 0, y: -10 }}
+          animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 2, delay: 1 }}
         >
           <UnderlineButton className='' href="/lecons"> Commencer </UnderlineButton>
@@ -35,5 +35,6 @@ export default function Page() {
 
       </div>
     </main>
+    
   );
 }
