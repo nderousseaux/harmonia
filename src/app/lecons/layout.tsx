e59f6b0d9df213e755b0c4e384618240b07e0839
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-03-03 18:16:53 +0100

    🎨 design: entry animation on home page

--- src/app/lecons/layout.tsx
+++ src/app/lecons/layout.tsx
@@ -1,11 +1,12 @@
-import * as motion from 'motion/react-client';
-import { Metadata } from 'next';
-import { Suspense } from 'react';
 import Link from 'next/link';
+import { Metadata } from 'next';
+import * as motion from 'motion/react-client';
 
 import "@/src/ui/global.css";
 import Logo from '@/src/ui/components/logo';
-import LeconsList, { LeconsListSkeleton } from '@/src/ui/components/nav/lecons-list';
+import NavItem from '@/src/ui/components/nav/nav-item';
+import { fetchLessons } from '@/src/lib/data';
+
 
 // Some metadata
 export const metadata: Metadata = {
@@ -15,15 +16,16 @@ export const metadata: Metadata = {
 
 
 // Navigation sidebar
-export default function Layout({ children }: { children: React.ReactNode; }) {
+export default async function Layout({ children }: { children: React.ReactNode; }) {
+	const lessons = await fetchLessons();
+
   return (
     <div className="flex h-screen">
-
-      <motion.nav
-				initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-				className="w-96 flex-none h-full flex-col pt-4 gap-4 bg-slate-700 bg-opacity-45"
+      <motion.nav 
+				initial={{ opacity: 0, y: -10 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5 }}
+				className="w-96 flex-none h-[110%] flex-col pt-4 gap-4 bg-slate-700 bg-opacity-45"
 			>
 				<Link
 					className="flex items-center justify-center h-32 shrink-0"
@@ -36,17 +38,19 @@ export default function Layout({ children }: { children: React.ReactNode; }) {
 						Mes leçons
 					</h2>
 
-					<Suspense fallback={<LeconsListSkeleton />}>
-						<LeconsList />
-					</Suspense>
+					<div className="overflow-y-auto h-full">
+						{lessons.map((lesson, index) => (
+							<NavItem key={index} isFirst={index === 0} lesson={lesson} />
+						))}
+					</div>
 				</div>
       </motion.nav>
 
-      <motion.div
+      <motion.div 
+				initial={{ opacity: 0, y: -10 }}
+				animate={{ opacity: 1, y: 0 }}
+				transition={{ duration: 0.5, delay: 0.5 }}
 				className="flex-grow"
-				initial={{ opacity: 0 }}
-				animate={{ opacity: 1 }}
-				transition={{ duration: 1, delay: 0.5 }}
 			>
 				{children}
 			</motion.div>
